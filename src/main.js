0d4280edde7c27d8e5bc0bf42e5e54e2c67c019f
Roman <roman.anokhin.work@gmail.com>
2025-10-17 03:00:07 +0300

    Update scroll-top

--- src/main.js
+++ src/main.js
@@ -6,20 +6,5 @@ import './js/feedbacks';
 import './js/events';
 import './js/contact-modal';
 import './js/modal-window-books';
+import './js/scroll-top';
 
-const scrollTopBtn = document.getElementById('scrollTopBtn');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 300) {
-    scrollTopBtn.classList.add('show');
-  } else {
-    scrollTopBtn.classList.remove('show');
-  }
-});
-
-scrollTopBtn.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-});
